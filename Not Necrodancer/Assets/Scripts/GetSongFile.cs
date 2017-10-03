13737e5a01184f3b8c530dd938e4a3b1d8b4bd67
Unknown <jonitasala@gmail.com>
2017-10-03 19:51:56 +0300

    Tried to do custom music and beat detection, don't do it

--- Not Necrodancer/Assets/Scripts/GetSongFile.cs
+++ Not Necrodancer/Assets/Scripts/GetSongFile.cs
@@ -5,15 +5,17 @@ using System.IO;
 
 public class GetSongFile : MonoBehaviour {
 
-    DirectoryInfo musicFolder;
-    WWW myClip;
-    string myPath;
+    public DirectoryInfo musicFolder;
+    public WWW myClip;
+    public string myPath;
+    public AudioProcessor _ap;
 
     AudioSource _as;
 
 	// Use this for initialization
 	void Start () {
-        myPath = "C:/Users/c5jtasal/Music/Custom music";
+        _as = GetComponent<AudioSource>();
+        myPath = "C:/Users/c5jtasal/Music/Custom music/ogg";
         musicFolder = new DirectoryInfo(myPath);
         myClip = new WWW("file:///" + musicFolder.GetFiles()[0].FullName);
         _as.clip = myClip.GetAudioClip(false, false);
@@ -22,9 +24,10 @@ public class GetSongFile : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (!_as.isPlaying)
+		if (!_as.isPlaying && _as.clip.loadState == AudioDataLoadState.Loaded)
         {
-            
+            _as.Play();
+            _ap.enabled = true;
         }
 	}
 }
